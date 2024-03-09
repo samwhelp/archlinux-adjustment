5fc2bcd75d1f5d4200b6ec4bf00f406aff75240f
samwhelp <samwhelp@users.noreply.github.com>
2024-03-09 20:14:13 +0800

    fix

--- _demo/archinstall/profile/locale/en_us/xfce/start.sh
+++ _demo/archinstall/profile/locale/en_us/xfce/start.sh
@@ -11,5 +11,3 @@
 
 
 archinstall --config demo-config.json --creds demo-permit.json
-
-##archinstall --config https://samwhelp.github.io/archlinux-adjustment/_demo/archinstall/profile/locale/en_us/xfce/demo-config.json --creds https://samwhelp.github.io/archlinux-adjustment/_demo/archinstall/profile/locale/en_us/xfce/demo-permit.json
