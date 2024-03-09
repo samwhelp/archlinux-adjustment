aaec28591f8b6dc65db235e938ffa945367dda8e
samwhelp <samwhelp@users.noreply.github.com>
2024-03-09 22:05:12 +0800

    fix

---
+++ sample/archinstall/profile/locale/en_us/xfce/start-remote.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+##
+## ## Link
+##
+## * https://github.com/archlinux/archinstall
+## * https://archinstall.archlinux.page/
+## * https://wiki.archlinux.org/title/archinstall
+##
+## ``` sh
+## bash <(curl -L https://raw.githubusercontent.com/samwhelp/archlinux-adjustment/gh-pages/_demo/archinstall/profile/locale/en_us/xfce/start-remote.sh)
+## ```
+##
+
+
+##archinstall --config demo-config.json --creds demo-permit.json
+
+
+archinstall --config https://raw.githubusercontent.com/samwhelp/archlinux-adjustment/gh-pages/_demo/archinstall/profile/locale/en_us/xfce/demo-config.json --creds https://raw.githubusercontent.com/samwhelp/archlinux-adjustment/gh-pages/_demo/archinstall/profile/locale/en_us/xfce/demo-permit.json
