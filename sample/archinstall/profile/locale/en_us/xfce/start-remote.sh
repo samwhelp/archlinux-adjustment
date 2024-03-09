63e5db450420cbd3595e8dd99570ffe7355fb400
samwhelp <samwhelp@users.noreply.github.com>
2024-03-09 22:14:26 +0800

    fix

--- sample/archinstall/profile/locale/en_us/xfce/start-remote.sh
+++ sample/archinstall/profile/locale/en_us/xfce/start-remote.sh
@@ -8,6 +8,8 @@
 ## * https://archinstall.archlinux.page/
 ## * https://wiki.archlinux.org/title/archinstall
 ##
+## ## Usage
+##
 ## ``` sh
 ## bash <(curl -L https://raw.githubusercontent.com/samwhelp/archlinux-adjustment/main/sample/archinstall/profile/locale/en_us/xfce/start-remote.sh)
 ## ```
