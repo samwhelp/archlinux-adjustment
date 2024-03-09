e3a2da418ba2017c1c2a4549656ce9c704accb8f
samwhelp <samwhelp@users.noreply.github.com>
2024-03-09 19:48:05 +0800

    fix

--- _demo/archinstall/profile/locale/en_us/xfce/start-remote.sh
+++ _demo/archinstall/profile/locale/en_us/xfce/start-remote.sh
@@ -8,8 +8,14 @@
 ## * https://archinstall.archlinux.page/
 ## * https://wiki.archlinux.org/title/archinstall
 ##
+## ``` sh
+## bash <(curl -L https://raw.githubusercontent.com/samwhelp/archlinux-adjustment/gh-pages/_demo/archinstall/profile/locale/en_us/xfce/start-remote.sh)
+## ```
+##
 
 
 ##archinstall --config demo-config.json --creds demo-permit.json
 
-archinstall --config https://samwhelp.github.io/archlinux-adjustment/_demo/archinstall/profile/locale/en_us/xfce/demo-config.json --creds https://samwhelp.github.io/archlinux-adjustment/_demo/archinstall/profile/locale/en_us/xfce/demo-permit.json
+##archinstall --config https://samwhelp.github.io/archlinux-adjustment/_demo/archinstall/profile/locale/en_us/xfce/demo-config.json --creds https://samwhelp.github.io/archlinux-adjustment/_demo/archinstall/profile/locale/en_us/xfce/demo-permit.json
+
+archinstall --config https://raw.githubusercontent.com/samwhelp/archlinux-adjustment/gh-pages/_demo/archinstall/profile/locale/en_us/xfce/demo-config.json --creds https://raw.githubusercontent.com/samwhelp/archlinux-adjustment/gh-pages/_demo/archinstall/profile/locale/en_us/xfce/demo-permit.json
