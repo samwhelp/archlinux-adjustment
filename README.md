4b8c38131687e19364a8a959b6b9750877ffe3f9
samwhelp <samwhelp@users.noreply.github.com>
2024-03-09 19:38:30 +0800

    fix

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 # Home
 
+| Link | GitHub |
+| ---- | ------ |
 | [ArchLinux Adjustment](https://samwhelp.github.io/archlinux-adjustment/) | [GitHub](https://github.com/samwhelp/archlinux-adjustment) |
---
+++ _demo/archinstall/profile/locale/en_us/xfce/start-remote.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+##
+## ## Link
+##
+## * https://github.com/archlinux/archinstall
+## * https://archinstall.archlinux.page/
+## * https://wiki.archlinux.org/title/archinstall
+##
+
+
+##archinstall --config demo-config.json --creds demo-permit.json
+
+archinstall --config https://samwhelp.github.io/archlinux-adjustment/_demo/archinstall/profile/locale/en_us/xfce/demo-config.json --creds https://samwhelp.github.io/archlinux-adjustment/_demo/archinstall/profile/locale/en_us/xfce/demo-permit.json
